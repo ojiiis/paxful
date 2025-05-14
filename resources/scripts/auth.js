02ee14a3a0283033d87600b3ec31e5a897c02d78
ojiiis <official.ojingirisamuel@gmail.com>
2025-05-14 19:04:47 +0100

    from dell

--- resources/scripts/auth.js
+++ resources/scripts/auth.js
@@ -1,8 +1,7 @@
-let params = getAllParams(url = window.location.href)
-console.log(params,)
+let params = getAllParams(url = window.location.href);
 if(params['a']!=undefined){
     [...document.getElementsByClassName("auth-sections")].forEach((elem)=>elem.style.display = "none");
-    document.getElementById(params['a']).style.display = "block";
+    document.getElementById(params['a']).style.display = "flex";
 }else{
-     document.getElementById("sign-up").style.display = "block";
+     document.getElementById("sign-in").style.display = "flex";
 }
