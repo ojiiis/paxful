bb4dfc8348c86732c8e0063f0d2c883b06ba5340
ojiiis <official.ojingirisamuel@gmail.com>
2025-05-15 22:44:24 +0100

    from dell

--- resources/scripts/auth.js
+++ resources/scripts/auth.js
@@ -2,6 +2,9 @@ let params = getAllParams(url = window.location.href);
 if(params['a']!=undefined){
     [...document.getElementsByClassName("auth-sections")].forEach((elem)=>elem.style.display = "none");
     document.getElementById(params['a']).style.display = "flex";
+    if(params['a'] == "sign-up" && params['email'] != undefined){
+document.getElementById('email').value = params['email'];
+    }
 }else{
      document.getElementById("sign-in").style.display = "flex";
 }
