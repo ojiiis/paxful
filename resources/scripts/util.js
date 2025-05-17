bc500aa70aa2e325e97130f2384f2b7eabbc694c
ojiiis <official.ojingirisamuel@gmail.com>
2025-05-17 18:29:32 +0100

    from dell

--- resources/scripts/util.js
+++ resources/scripts/util.js
@@ -15,4 +15,24 @@
 }
   document.getElementById("close-cookie-pop").onclick = () => document.getElementById("cookie-pop").remove();
   document.getElementById("close-qr-code").onclick = () => document.getElementById("qr-code").remove();
-  
+  const tr = document.querySelectorAll('#app-right .table-row');
+  if (tr.length) {
+  tr[tr.length - 1].style.borderBottom = 'none';
+}
+
+if(document.getElementById("copy-address")){
+ document.getElementById("copy-address").onclick = function(){
+      const originalInput = document.getElementById("bitcoin-address");
+  const tempInput = document.createElement("input");
+  tempInput.value = originalInput.value;
+
+  document.body.appendChild(tempInput);
+  tempInput.select();
+  tempInput.setSelectionRange(0, 99999); // Mobile support
+  document.execCommand("copy");
+  document.body.removeChild(tempInput);
+
+    // Optional: Show confirmation
+  //  alert("Copied: " + input.value);
+   }
+  }
