296f07261bcbaf7395c1a62eca2f9c2270a3d74e
ojiiis <official.ojingirisamuel@gmail.com>
2025-06-05 23:38:45 +0100

    thursday night

--- resources/scripts/var.js
+++ resources/scripts/var.js
@@ -1,3 +1,3 @@
-//const api = "http://127.0.0.1:3000";
-const api = "https://lin.com.ng/ocoin/paxful";
+const api = "http://127.0.0.1:3000";
+//const api = "https://lin.com.ng/ocoin/paxful";
 const coins = {eth:"Ethereum",btc:"Bitcoin",ltc:"Litecoin",usdt:"Tether"};
