4dc915bd64a1ff29814daa23a5a6c62fe9cb82cb
ojiiis <official.ojingirisamuel@gmail.com>
2025-07-31 00:06:48 +0100

    new sun

--- resources/scripts/util.js
+++ resources/scripts/util.js
@@ -371,7 +371,7 @@ function createOfferDetails(data) {
             const box = el("div", "gbtp-notes-box");
             box.textContent = data.tradeTerms || "—";
             stat.appendChild(box);
-        } else if(label === "Payment Duration" && data.url && data.status == null){
+        } else if(label === "Payment Duration" && data.url && data.status == ""){
            const e = document.createElement("span");
            e.className = "gbtp-value";
            stat.appendChild(e);
